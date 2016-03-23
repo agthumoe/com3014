1eaf4f7b69568d2de74a774b195539e40807ff4e
Aung Thu Moe <ta00097@surrey.ac.uk>
2016-03-23 12:26:46 +0000

    fixed websocket message type mismatch issue

--- src/main/java/com/surrey/com3014/group5/dto/HelloMessage.java
+++ src/main/java/com/surrey/com3014/group5/dto/HelloMessage.java
@@ -1,9 +1,6 @@
 package com.surrey.com3014.group5.dto;
 
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHeaders;
-
-public class HelloMessage implements Message<String> {
+public class HelloMessage {
 
     private String name;
 
@@ -11,13 +8,4 @@ public class HelloMessage implements Message<String> {
         return name;
     }
 
-    @Override
-    public String getPayload() {
-        return null;
-    }
-
-    @Override
-    public MessageHeaders getHeaders() {
-        return null;
-    }
 }
