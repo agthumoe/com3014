04e6f176827d305ab1ebd9205c37e6f1678629ea
Spyros Balkonis <Spyros Balkonis>
2016-03-26 18:13:02 +0200

    Adjustments to the chat service

--- src/main/java/com/surrey/com3014/group5/controllers/ChatController.java
+++ src/main/java/com/surrey/com3014/group5/controllers/ChatController.java
@@ -1,12 +1,13 @@
 package com.surrey.com3014.group5.controllers;
 
-import com.surrey.com3014.group5.dto.Greeting;
-import com.surrey.com3014.group5.dto.HelloMessage;
+import com.surrey.com3014.group5.dto.ChatMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+
+import java.sql.Timestamp;
 
 /**
  * @author Aung Thu Moe
@@ -18,7 +19,7 @@ public class ChatController {
 
     @MessageMapping("/websocket/tracker")
     @SendTo("/topic/global.chat")
-    public Greeting greeting(HelloMessage message) throws Exception {
-        return new Greeting("Hello, " + message.getName() + "!");
+    public String greeting(ChatMessage message) throws Exception {
+        return "[" + new Timestamp(System.currentTimeMillis()) + "]: " + message.getMessage();
     }
 }
---
+++ src/main/java/com/surrey/com3014/group5/dto/ChatMessage.java
@@ -0,0 +1,11 @@
+package com.surrey.com3014.group5.dto;
+
+public class ChatMessage {
+
+    private String message;
+
+    public String getMessage() {
+        return this.message;
+    }
+
+}
