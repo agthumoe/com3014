b4e34d56812c0576048f3d52d4350827e6018d09
Chris Doherty <chris.doherty4@gmail.com>
2016-04-11 17:15:52 +0100

    Added handlnig of each command/response when challenging

--- src/main/webapp/assets/libs/tron.challenge.js
+++ src/main/webapp/assets/libs/tron.challenge.js
@@ -22,6 +22,11 @@ $(function () {
             _challengeStomp: null,
             
             /**
+             * A reference to the challenge modal to accept/dencline invitations.
+             */
+            _challengeModal: null,
+            
+            /**
              * TronChallenge.instance
              * Creates a new instance of the TronChallenge class.
              * 
@@ -39,6 +44,9 @@ $(function () {
              */
             init: function (username) {
                 var that = this;
+                
+                this._$challengeModal = $('#challenge-modal');
+                
                 this._challengeSocket = new SockJS(this._challengesQueue);
                 this._challengeStomp = Stomp.over(this._challengeSocket);
                 var challengeStomp = this._challengeStomp;
@@ -47,6 +55,13 @@ $(function () {
                     challengeStomp.subscribe(that._getSubscriptionURL(username), function (response) {
                         that.handleCommand(response);
                     });
+                });
+                
+                $('#btn-challenge-accept').on('click', function (e) {
+                    that.accept();
+                });
+                $('#btn-challenge-deny').on('click', function (e) {
+                    that.decline();
                 });
                 
                 return this;
@@ -70,20 +85,105 @@ $(function () {
              * @returns void
              */
             handleCommand: function (response) {
-                var body = JSON.parse(response.body);
+                response = JSON.parse(response.body);
                 
-                if (body.command === 'CHALLENGE.NEW') {
-                    var $challengeModal = $('#challenge-modal');
-                    var $modalBody = $challengeModal.find('[class=modal-body]');
-                    $modalBody.html(body.challenger.name + " has challenged you!");
-                    $challengeModal.show();
-                } else if (body.command === 'CHALLENGE.ACCEPT') {
-                    
-                } else if (body.command === 'CHALLENGE.DENY') {
-                    
-                } else if (body.command === 'CHALLENGE.TIMEOUT') {
-                    
+                if (response.command === 'CHALLENGE.NEW') {
+                    this._commandNewChallenge(response);
+                } else if (response.command === 'CHALLENGE.ACCEPT') {
+                    this._commandAcceptedChallenge(response);
+                } else if (response.command === 'CHALLENGE.DECLINE') {
+                    this._commandDeniedChallenge(response);
+                } else if (response.command === 'CHALLENGE.TIMEOUT') {
+                    this._commandTimeoutChallenge(response);
                 }
+            },
+            
+            /**
+             * TronChallenge._comandNewChallenge
+             * Handles other people's invitations to start a game.
+             * 
+             * @param Object response
+             * @returns this
+             */
+            _commandNewChallenge: function (response) {
+                this._$challengeModal.attr('data-game-id', response.gameID);
+                var $modalBody = this._$challengeModal.find('[class=modal-body]');
+                $modalBody.html(response.challenger.name + " has challenged you!");
+                this._$challengeModal.modal({show: true});
+                
+                return this;
+            },
+            
+            /**
+             * TronChallenge._commandAcceptedChallenge
+             * Handles other people accepting challenges this client has made.
+             * 
+             * @param Object response
+             * @returns this
+             */
+            _commandAcceptedChallenge: function (response) {
+                var $notification = $('<div />').addClass('alert alert-success')
+                    .html('<strong>' + response.challenged.name + '</strong> accepted your ' +
+                        'invitation. Prepare to play!')
+                    .css({
+                        display: 'none'
+                    });
+                $('body > div.container').prepend($notification);
+                $notification.slideDown();
+                setTimeout(function () {
+                    $notification.slideUp();
+                }, 3000);
+                
+                return this;
+            },
+            
+            /**
+             * TronChallenge._commandDeniedChallenge
+             * Handles other clients denying of our challenges.
+             * 
+             * @param Object response
+             * @returns this
+             */
+            _commandDeniedChallenge: function (response) {
+                var $notification = $('<div />').addClass('alert alert-warning')
+                    .html('<strong>' + response.challenged.name + '</strong> declined your ' +
+                        'invitation.')
+                    .css({
+                        display: 'none'
+                    });
+                $('body > div.container').prepend($notification);
+                $notification.slideDown();
+                setTimeout(function () {
+                    $notification.slideUp();
+                }, 3000);
+                
+                return this;
+            },
+            
+            /**
+             * TronChallenge._commandTimeoutChallenge
+             * Handles timing out of a challenge invitation. 
+             * 
+             * @param Object response
+             * @returns this
+             */
+            _commandTimeoutChallenge: function (response) {
+                var $notification = $('<div />').addClass('alert alert-danger')
+                    .css({
+                        display: 'none'
+                    });
+                if (response.challenger.name === User.name) {
+                    $notification.html('Your challenge against <strong>' 
+                        + response.challenged.name + '</strong> ' + 
+                        'timed out.');
+                }
+                $('body > div.container').prepend($notification);
+                $notification.slideDown();
+                setTimeout(function () {
+                    $notification.slideUp();
+                }, 3000);
+                
+                return this;
             },
             
             /**
@@ -107,12 +207,42 @@ $(function () {
                 return this;
             },
 
-            accept: function (challengeID) {
-
+            /**
+             * TronChallenge.accept
+             * Handles the acceptance of a challenge invitation
+             * 
+             * @returns this
+             */
+            accept: function () {
+                var gameID = this._$challengeModal.attr('data-game-id');
+                this._challengeStomp.send(TronChallenge._challengesQueue, {}, JSON.stringify({
+                    command: 'CHALLENGE.ACCEPT',
+                    data: {
+                        gameID: gameID
+                    }
+                }));
+                
+                this._$challengeModal.modal('hide');
+                
+                return this;
             },
 
-            decline: function (challengeID) {
-
+            /**
+             * TronChallenge.decline
+             * Handles declining of an invitation.
+             * 
+             * @returns this
+             */
+            decline: function () {
+                var gameID = this._$challengeModal.attr('data-game-id');
+                this._challengeStomp.send(TronChallenge._challengesQueue, {}, JSON.stringify({
+                    command: 'CHALLENGE.DECLINE',
+                    data: {
+                        gameID: gameID
+                    }
+                }));
+                
+                return this;
             }
         };
 
