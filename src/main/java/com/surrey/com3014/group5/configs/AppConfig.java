13b6fade0ff061bc3229f6cd329226471f6b013b
Aung Thu Moe <ta00097@surrey.ac.uk>
2016-04-13 13:55:59 +0100

    removed unnecessary beans

--- src/main/java/com/surrey/com3014/group5/configs/AppConfig.java
+++ src/main/java/com/surrey/com3014/group5/configs/AppConfig.java
@@ -1,50 +1,53 @@
 package com.surrey.com3014.group5.configs;
 
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.encrypt.Encryptors;
-import org.springframework.security.crypto.encrypt.TextEncryptor;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 import java.time.format.DateTimeFormatter;
 
 /**
+ * Initialise necessary beans for the application
+ *
  * @author Aung Thu Moe
  */
 @Configuration
 public class AppConfig {
 
-    @Bean
-    public ServerProperties getServerProperties() {
-        return new ServerCustomization();
-    }
-
+    /**
+     * Initialise {@link org.springframework.security.core.session.SessionRegistry} bean which contains session
+     * information.
+     *
+     * @return {@link org.springframework.security.core.session.SessionRegistry} containing SessionInformation
+     * @see SessionRegistry
+     */
     @Bean
     public SessionRegistry sessionRegistry() {
         return new SessionRegistryImpl();
     }
 
-    @Bean
-    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
-        return new ServletListenerRegistrationBean<>(new HttpSessionEventPublisher());
-    }
-
+    /**
+     * Initialise a {@link org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder} as a
+     * {@link org.springframework.security.crypto.password.PasswordEncoder} bean.
+     *
+     * @return {@link org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder} as a
+     * {@link org.springframework.security.crypto.password.PasswordEncoder} to encrypt password.
+     * @see PasswordEncoder
+     * @see BCryptPasswordEncoder
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public TextEncryptor textEncryptor() {
-        return Encryptors.noOpText();
-    }
-
+    /**
+     * Initialise a {@link java.time.format.DateTimeFormatter} bean to format date time.
+     *
+     * @return {@link java.time.format.DateTimeFormatter} to format date time.
+     */
     @Bean
     public DateTimeFormatter dateTimeFormatter() {
         return DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
--- src/main/java/com/surrey/com3014/group5/configs/ServerCustomization.java
+++ src/main/java/com/surrey/com3014/group5/configs/ServerCustomization.java
@@ -4,12 +4,21 @@ import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 
 /**
+ * Server customisation for web application.
+ *
  * @author Aung Thu Moe
  */
+@Component
 public class ServerCustomization extends ServerProperties {
 
+    /**
+     * Customise error pages.
+     *
+     * @param container Servelet container to customise error pages.
+     */
     @Override
     public void customize(ConfigurableEmbeddedServletContainer container) {
         super.customize(container);
