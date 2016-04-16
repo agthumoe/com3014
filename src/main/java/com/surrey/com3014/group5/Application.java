99db26883f26f935640cf03f2ec66c78c3a8e881
Aung Thu Moe <ta00097@surrey.ac.uk>
2016-04-16 03:07:48 +0100

    moved properties files from META-INF folder into config folder

--- src/main/java/com/surrey/com3014/group5/Application.java
+++ src/main/java/com/surrey/com3014/group5/Application.java
@@ -20,28 +20,34 @@ import java.net.UnknownHostException;
  */
 @ComponentScan
 @EnableAutoConfiguration
-@PropertySource(value = { "classpath:META-INF/application.properties", "classpath:META-INF/database.properties",
-		"classpath:META-INF/swagger.properties" })
+@PropertySource(value = {
+    "classpath:config/database.properties",
+    "classpath:config/swagger.properties"
+})
 public class Application extends SpringBootServletInitializer {
-	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class.getName());
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class.getName());
 
-	/**
-	 * Configure this application as a spring-boot application.
-	 */
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
+    /**
+     * Configure this application as a spring-boot application.
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
-	/**
-	 * Entry point of the application.
-	 */
-	public static void main(String[] args) throws UnknownHostException {
-		LOGGER.info("starting tron application");
-		Environment env = SpringApplication.run(Application.class, args).getEnvironment();
-
-		LOGGER.info("Access URLs:\t\thttp://127.0.0.1:8080");
-		LOGGER.info("Active Profiles:\t\t{}", StringUtils.arrayToDelimitedString(env.getActiveProfiles(), ", "));
-	}
+    /**
+     * Entry point of the application.
+     */
+    public static void main(String[] args) throws UnknownHostException {
+        Environment env = SpringApplication.run(Application.class, args).getEnvironment();
+        if (env.containsProperty("server.port")) {
+            LOGGER.info("Access URLs:\t\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+        } else {
+            LOGGER.info("Access URLs:\t\thttp://127.0.0.1:8080");
+        }
+        if (env.getActiveProfiles().length != 0) {
+            LOGGER.info("Active Profiles:\t\t{}", StringUtils.arrayToDelimitedString(env.getActiveProfiles(), ", "));
+        }
+    }
 
 }
